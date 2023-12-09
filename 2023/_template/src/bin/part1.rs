c195e5861e903532fe62ae89e5959811c7413ffc
Dustin Knie <dustin@nulldomain.com>
2023-12-08 19:07:58 -0800

    Add template to use for each days solution

---
+++ 2023/_template/src/bin/part1.rs
@@ -0,0 +1,26 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
+mod common;
+
+fn main() {
+    let input = common::read_lines("input.txt").expect("Unable to open input");
+    let ans = process(input);
+    println!("Part 1: {}", ans);
+}
+
+fn process(mut input: impl Iterator<Item = String>) -> usize {
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_sample_test() {
+        let input = common::read_lines("sample_one.txt").expect("Unable to open sample text");
+        assert_eq!(0, process(input));
+    }
+}
